f6a5b2c1330e44448427e7778012b02e4e2f7458
Violetta Stolarova <violettastolarova@icloud.com>
2024-08-28 22:53:00 +0300

    feat: add import alias configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import react from 'eslint-plugin-react'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import react from 'eslint-plugin-react';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.recommended,
       eslintPluginPrettierRecommended,
     ],
     files: ['**/*.{ts,tsx}'],
@@ -29,13 +29,10 @@ export default tseslint.config(
       react,
     },
     rules: {
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       ...reactHooks.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
     },
   },
   { ignores: ['dist'] }
-)
+);
