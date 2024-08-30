4b51e1317ccb505ac859eb353f873881f3a614ae
Violetta Stolarova <violettastolarova@icloud.com>
2024-08-30 11:19:35 +0300

    refactor: update file  with prettier

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -7,7 +7,11 @@ const AppRouter = () => (
   <Suspense fallback="">
     <Routes>
       {Object.values(routeConfig).map(({ element, path }) => (
-        <Route key={path} path={path} element={<div className="page-content">{element}</div>} />
+        <Route
+          key={path}
+          path={path}
+          element={<div className="page-content">{element}</div>}
+        />
       ))}
     </Routes>
   </Suspense>
