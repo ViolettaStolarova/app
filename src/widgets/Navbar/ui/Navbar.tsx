f379b28a2733ea4baf177ccb0e44a6177d143c3c
Violetta Stolarova <violettastolarova@icloud.com>
2024-09-01 10:02:04 +0300

    refactor: update Navbar structure

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,47 +1,31 @@
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 
-import { getUserAuthState, userActions } from 'entities/User';
-import { getRouteLogin, getRouteMain } from 'shared/const/router';
+import { getRouteMain } from 'shared/constants/router';
 import { Button } from 'shared/ui/Button';
 import { Text } from 'shared/ui/Text';
-import { ThemeSwitcher } from 'features/ThemeSwitcher';
-import { AppLink } from 'shared/ui/AppLink/ui/AppLink';
+import { AppLink } from 'shared/ui/AppLink';
 import { HStack } from 'shared/ui/Stack';
+import { User } from 'entities/User';
+import { ThemeSwitcher } from 'features/ThemeSwitcher';
 
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
+  userAuthData?: User;
+  onLoginButtonClick: () => void;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-
-  const userAuthData = useSelector(getUserAuthState);
-
-  const onLogout = useCallback(() => {
-    dispatch(userActions.logout());
-  }, [dispatch]);
-
-  const onLoginButtonClick = useCallback(() => {
-    if (userAuthData) {
-      onLogout();
-    } else {
-      navigate(getRouteLogin());
-    }
-  }, [navigate, onLogout, userAuthData]);
+export const Navbar = (props: NavbarProps) => {
+  const { className, onLoginButtonClick, userAuthData } = props;
 
   return (
     <header className={classNames(cls.Navbar, {}, [className])}>
       <AppLink variant="primaryInverted" to={getRouteMain()}>
         <Text bold variant="accent" size="m" title="Best Application" />
       </AppLink>
-      <HStack className={cls.controlsContainer} gap={16}>
+
+      <HStack className={cls.controlsContainer} gap={16} align="center">
         <ThemeSwitcher />
         <Button size="l" variant="primary" onClick={onLoginButtonClick}>
           {userAuthData ? 'Log Out' : 'Sign In'}
