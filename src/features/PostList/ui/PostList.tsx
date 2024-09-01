3f11fdc44e3c4c9f0e070550050652116de88693
Violetta Stolarova <violettastolarova@icloud.com>
2024-09-01 09:51:06 +0300

    feat: add PostList feature

---
+++ src/features/PostList/ui/PostList.tsx
@@ -0,0 +1,47 @@
+import { memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import { HStack } from 'shared/ui/Stack';
+import { Text } from 'shared/ui/Text';
+import { PostItem } from 'entities/Post';
+import { usePostList } from '../hooks/usePostList';
+
+import cls from './PostList.module.scss';
+
+type Props = {
+  className?: string;
+};
+
+export const PostList = memo((props: Props) => {
+  const { className } = props;
+
+  const { error, isLoading, posts } = usePostList();
+
+  const rootContainer = classNames(cls.PostList, {}, [className]);
+
+  if (isLoading) {
+    return (
+      <HStack className={rootContainer} justify="center">
+        <Text bold size="l" title="Loading..." />
+      </HStack>
+    );
+  }
+
+  if (error) {
+    return (
+      <HStack className={rootContainer} justify="center" align="center">
+        <Text bold size="l" title="Something went wrong :(" />
+      </HStack>
+    );
+  }
+
+  return (
+    <HStack className={rootContainer} justify="center" wrap="wrap" gap={32}>
+      {posts && posts?.length > 0 ? (
+        posts.map((item) => <PostItem key={item.id} item={item} />)
+      ) : (
+        <Text size="m" title="No posts available" />
+      )}
+    </HStack>
+  );
+});
