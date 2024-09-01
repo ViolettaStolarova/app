5f51da27627370135b20a55e1c0a495ec7e9dcf3
Violetta Stolarova <violettastolarova@icloud.com>
2024-09-01 16:48:17 +0300

    refactor: delete StoreProvider

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
-import { StoreProvider } from 'app/providers/StoreProvider';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from 'app/providers/StoreProvider';
 
 import App from 'app/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <StoreProvider>
+    <ReduxProvider store={store}>
       <BrowserRouter>
         <ThemeProvider>
           <App />
         </ThemeProvider>
       </BrowserRouter>
-    </StoreProvider>
+    </ReduxProvider>
   </StrictMode>
 );
