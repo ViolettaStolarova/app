d08e3b66e0ea5fc474ff85f41549ca7e5cf87ec3
Violetta Stolarova <violettastolarova@icloud.com>
2024-09-02 20:22:42 +0300

    feat: configure vite for docker

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       app: path.resolve(__dirname, './src/app'),
